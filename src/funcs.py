366619681f1c00fbdd6da50a17a6cf3c72475df6
isa <isalonglegs@gmail.com>
2022-10-13 22:23:41 -0700

    first commit

---
+++ src/funcs.py
@@ -0,0 +1,42 @@
+#!/bin/python
+"""
+Drawing Functions
+"""
+if __name__ == "__main__":
+    print('dont run this file !!')
+    exit()
+
+def _prepare(t, origin: tuple):
+    """
+    create origin coordinates and create length and width values
+    also prepare the turtles color and starting coordinates
+    takes turtle object and origin coordinates
+    """
+    t.up()
+    ogX, ogY = origin
+    x = 20
+    y = 50
+    t.goto(ogX, ogY)
+    t.color('white')
+    return (ogX, ogY, x, y)
+
+
+def draw_zero(t, origin: tuple = (0, 0)):
+    """
+    Draws a zero with origin(tuple) coordinates
+    takes in a turtle object
+    """
+
+    ogX, ogY, x, y = _prepare(t, origin)
+    t.pendown()
+    t.goto(ogX, y)
+    t.goto(ogX-30, y)
+    t.goto(ogX-30, ogY)
+    t.goto(ogX, ogY)
+
+
+def draw_one(t, origin: tuple = (0, 0)):
+    ogX, _, _, y = _prepare(t, origin)
+    bottomLineLength = 40
+    t.pendown()
+    t.goto(ogX, y)
---
+++ src/main.py
@@ -0,0 +1,44 @@
+#!/bin/python
+from turtle import *
+from funcs import *
+from random import randint
+
+n_bits = int(input("how many bits to draw (whole number): "))
+bgcolor("blue")
+title("Random Binary Num")
+t = Turtle()
+t.width(10)
+
+
+"""
+Will use a while/for loop to generate the origin values hopefully from right to left
+"""
+
+
+bits = []
+for i in range(n_bits):
+    bits.append(randint(0, 1))
+i = 0
+used_i = []
+# print(bits)
+for x in range(400, -400, -50):
+    if i in used_i:
+        break
+    if i > len(bits) - 1:
+        i = 0
+        continue
+    if bits[i] == 1:
+        draw_one(t, (x, 0))
+    else:
+        draw_zero(t, (x, 0))
+    used_i.append(i)
+    i += 1
+
+num = ""
+for bit in bits:
+    num = num + str(bit)
+num = int(num, 2)
+
+
+print("The Number Was:", num)
+done()
