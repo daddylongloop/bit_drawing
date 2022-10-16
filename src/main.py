0a161bc673c9b9eb9cc5d007dca91081d7867346
isa <isalonglegs@gmail.com>
2022-10-16 16:14:39 -0700

    changes in logic not very thought through, still made the spacing a little better than before

--- src/main.py
+++ src/main.py
@@ -9,24 +9,28 @@ title("Random Binary Num")
 t = Turtle()
 t.width(10)
 
+
 """
 Will use a while/for loop to generate the origin values hopefully from right to left
 """
 
 
 bits = []
-for _ in range(n_bits):
+for i in range(n_bits):
     bits.append(randint(0, 1))
 i = 0
-y = 0
-for x in range(400, -400, -30):
+used_i = []
+for x in range(400, -400, -50):
+    if i in used_i:
+        break
     if i > len(bits) - 1:
         i = 0
-        break
+        continue
     if bits[i] == 1:
-        draw_one(t, (x, y))
+        draw_one(t, (x, 0))
     else:
-        draw_zero(t, (x, y))
+        draw_zero(t, (x+20, 0))
+    used_i.append(i)
     i += 1
 
 num = ""
